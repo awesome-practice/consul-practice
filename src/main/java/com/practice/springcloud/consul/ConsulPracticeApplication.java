8cff3b5df7bb51ce6c997c35eb9c98be963b6f0a
LeonGeroge <leongeroge@gmail.com>
2019-09-26 00:58:47 +0800

    consul-practice

---
+++ src/main/java/com/practice/springcloud/consul/ConsulPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.practice.springcloud.consul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsulPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsulPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/springcloud/consul/ConsulPracticeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.practice.springcloud.consul;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConsulPracticeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
